26b6e5bede0f14a071ab64e1a898117e4564b6d0
kamranss <kamranss@code.edu.az>
2023-03-19 14:38:59 +0400

    Feature: added new task JQuery Ajax API request

---
+++ assets/script/ajax.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+
+    $(document).on("keyup", "input", function(){
+        console.log($(this).val());
+    })
+    
+
+    $.ajax({
+        url: "http://api.aladhan.com/v1/calendar/2017/4?latitude=51.508515&longitude=-0.1254872&method=2",
+        method: "get",
+        success: function(data){
+            console.log(data);
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    })
+
+
+
+})
